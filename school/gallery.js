d8c58a8cc8fe5a33d533d757daad2ee1a55dac33
AgayevElmin <agayevelmin73@gmail.com>
2023-08-27 23:01:09 +0400

    TTM

--- school/gallery.js
+++ school/gallery.js
@@ -2,13 +2,13 @@ const galleryContainer = document.getElementById('gallery_container')
 
 window.onload = getGalleryData
 async function getGalleryData() {
-    const url = '../gallery.json'
+    const url = 'gallery.json'
     const stringData = await fetch(url)
     const JSONData = await stringData.json()
     try {
         for (let i = 0; i < JSONData.length; i++) {
             galleryContainer.innerHTML +=
-            `
+                `
             <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 gallery-item">
             <img src="${JSONData[i].Img}" alt="${JSONData[i].Name}">
         </div>
